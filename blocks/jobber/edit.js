1179945aae2ba8a03147113775bb5dd3264a9f16
faisal-alvi <faisalalvi33@gmail.com>
2025-03-27 18:48:54 +0530

    test load clients data in block

--- blocks/jobber/edit.js
+++ blocks/jobber/edit.js
@@ -6,6 +6,28 @@ import './editor.scss';
 
 export default function Edit({ attributes, setAttributes }) {
 	const { embedCode } = attributes;
+	const [data, setData] = useState(null);
+	const [loading, setLoading] = useState(true);
+	const [error, setError] = useState(null);
+
+	useEffect(() => {
+		fetch('/wp-json/jobber/v1/clients/?param1=test')
+			.then((response) => {
+				if (!response.ok) {
+					throw new Error(__('Failed to fetch data', 'jobber'));
+				}
+				return response.json();
+			})
+			.then((json) => {
+				setData(json);
+				setLoading(false);
+			})
+			.catch((err) => {
+				console.log(err.message);
+				setError(err.message);
+				setLoading(false);
+			});
+	}, []);
 
 	return (
 		<div {...useBlockProps()}>
