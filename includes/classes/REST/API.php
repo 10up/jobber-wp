de16c4e8028e21fae4adfaf5b408e15342f0650e
faisal-alvi <faisalalvi33@gmail.com>
2025-04-09 03:19:36 +0530

    update and handle the endpoints

--- includes/classes/REST/API.php
+++ includes/classes/REST/API.php
@@ -49,33 +49,85 @@ class API {
 	public function register_routes() {
 		// Register the routes here.
 
-		// Get clients
+		// Get Form.
 		register_rest_route(
 			self::$namespace,
-			'/clients',
+			'/get_form',
 			[
 				'methods'             => 'GET',
-				'callback'            => [ $this, 'get_clients' ],
-				'permission_callback' => [ $this, 'validate_token' ],
+				'callback'            => [ $this, 'get_form' ],
+				'permission_callback' => [ $this, 'has_permission' ],
+				// 'permission_callback' => '__return_true',
 				'args'                => [
-					'param1'  => [
+					'form_type'  => [
 						'type'     => 'string',
 						'required' => true,
 					],
 				],
 			]
 		);
+
+		// Get clients.
+		register_rest_route(
+			self::$namespace,
+			'/clients',
+			[
+				'methods'             => 'GET',
+				'callback'            => [ $this, 'get_clients' ],
+				'permission_callback' => [ $this, 'has_permission' ],
+			]
+		);
 	}
 
 	/**
-	 * Validate the Jobber Token.
+	 * Check if the user has permission to access the route.
+	 *
+	 * @return bool
+	 */
+	public function has_permission( \WP_REST_Request $request ) {
+		return true;
+	}
+
+	/**
+	 * Get the form from Jobber.
 	 *
 	 * @param WP_REST_Request $request The REST Request.
-	 * @return boolean
+	 * @return WP_REST_Response
 	 */
-	public function validate_token( \WP_REST_Request $request ) {
-		$token = new Token();
-		return $token->validate_token( $request, true );
+	public function get_form( $request ) {
+		$form_type = $request->get_param( 'form_type' );
+	
+		if ( empty( $form_type ) ) {
+			$form_type = 'request';
+		}
+	
+		$jobber = new \Jobber\Jobber();
+		$form   = $jobber->get_form( $form_type );
+	
+		if ( is_wp_error( $form ) ) {
+			return rest_ensure_response( $form );
+		}
+	
+		// Pull just the iframe/embed URL
+		$url = '';
+
+		if ( 'request' === $form_type && isset( $form['data']['requestSettings']['requestUrl'] ) ) {
+			$url = $form['data']['requestSettings']['requestUrl'];
+		} elseif ( 'booking' === $form_type && isset( $form['data']['onlineBookingConfiguration']['bookingUrl'] ) ) {
+			$url = $form['data']['onlineBookingConfiguration']['bookingUrl'];
+		}
+	
+		if ( empty( $url ) ) {
+			return new \WP_Error( 'no_url_found', __( 'No valid form URL found.', 'jobber' ), [ 'status' => 500 ] );
+		}
+	
+		return rest_ensure_response(
+			[
+				'form' => [
+					'iframeUrl' => esc_url_raw( $url ),
+				],
+			]
+		);
 	}
 
 	/**
