e461ccc04143f3e801d23d2ea703b8e436ab230c
faisal-alvi <faisalalvi33@gmail.com>
2025-04-14 20:02:50 +0530

    remove the deleted file's inclusion

--- blocks/forms/index.js
+++ blocks/forms/index.js
@@ -11,15 +11,6 @@ import save from './save';
 import metadata from './block.json';
 
 /**
- * Lets webpack process CSS, SASS or SCSS files referenced in JavaScript files.
- * All files containing `style` keyword are bundled together. The code used
- * gets applied both to the front of your site and to the editor.
- *
- * @see https://www.npmjs.com/package/@wordpress/scripts#using-css
- */
-import './style.scss';
-
-/**
  * Register new block type.
  */
 registerBlockType( metadata.name, {
