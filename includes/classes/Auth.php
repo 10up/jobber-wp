f12940bb232b8f6b1a3a691ae2f9b0e121ebbf6c
Tyler Bailey <tyler.bailey@10up.com>
2025-04-22 09:50:00 -0400

    Add missing Auth.php file

---
+++ includes/classes/Auth.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Jobber Authorization Flow
+ *
+ * @package Jobber
+ */
+
+namespace Jobber;
+
+/**
+ * Base class for Jobber Authorization Flow
+ */
+class Auth {
+	/**
+	 * Jobber Auth Middleware URL
+	 *
+	 * @todo Replace this with the actual middleware URL when available.
+	 * @var string
+	 */
+	public static $url = 'http://localhost:8000/auth';
+
+	/**
+	 * Determine if the user is authorized.
+	 *
+	 * @return boolean
+	 */
+	public static function is_authorized() {
+		$settings = \Jobber\Admin\Settings::get_settings();
+		if ( empty( $settings['authenticated'] ) ) {
+			return false;
+		}
+
+		return $settings['authenticated'];
+	}
+
+	/**
+	 * Get the Jobber API Token(s)
+	 *
+	 * @param string $token The token to get. access or refresh.
+	 * @return string Decrypted token.
+	 */
+	public static function get_token( $token = 'access' ) {
+		$settings = \Jobber\Admin\Settings::get_settings();
+		$token    = false !== strpos( $token, '_token' ) ? $token : "{$token}_token";
+
+		if ( empty( $settings[ $token ] ) ) {
+			return '';
+		}
+
+		// Decrypt token.
+		return $settings[ $token ];
+	}
+}
