7caeef90d02e2acefdb5caa549f1867596f36cc9
faisal-alvi <faisalalvi33@gmail.com>
2025-04-23 23:07:57 +0530

    add config file

---
+++ tests/utils/config/config.ts
@@ -0,0 +1,12 @@
+export const WP_ADMIN_USER = {
+  username: 'admin',
+  password: 'password'
+};
+
+export const WP_ADMIN_PASSWORD = 'password';
+
+export const BASE_URL = 'http://localhost:8889';
+
+export const TEST_TIMEOUT = 60000; // 60 seconds
+
+export const TEST_RETRIES = 2;
