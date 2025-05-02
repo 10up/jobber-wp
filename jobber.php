412815c5d2e9767c8c0b6029ed175f2d35a585e6
Jeffrey Paul <jeffpaul@hotmail.com>
2025-05-02 13:50:55 -0500

    fix plugin version refs to 1.0.0

--- jobber.php
+++ jobber.php
@@ -16,7 +16,7 @@
  */
 
 // Useful global constants.
-define( 'JOBBER_PLUGIN_VERSION', '0.1.0' );
+define( 'JOBBER_PLUGIN_VERSION', '1.0.0' );
 define( 'JOBBER_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 define( 'JOBBER_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 define( 'JOBBER_PLUGIN_INC', JOBBER_PLUGIN_PATH . 'includes/' );
