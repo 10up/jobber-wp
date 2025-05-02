3a2b340884504c916a42ae6ed2565a98a6dbcf58
Jeffrey Paul <jeffpaul@hotmail.com>
2025-05-02 13:42:51 -0500

    set jobber as author, primary contributor

--- jobber.php
+++ jobber.php
@@ -6,8 +6,8 @@
  * Version:           1.0.0
  * Requires at least: 6.6
  * Requires PHP:      7.4
- * Author:            10up
- * Author URI:        https://10up.com
+ * Author:            jobberintegrations
+ * Author URI:        https://www.getjobber.com/
  * License:           GPL v2 or later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       jobber
